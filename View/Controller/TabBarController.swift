f85d266996218d1804fcf7d5b217223fdff9e0a7
Abigail <abigaby0508@gmail.com>
2025-12-17 23:08:18 -0500

    Add SplashViewController, login style and bottom navigation bar

---
+++ View/Controller/TabBarController.swift
@@ -0,0 +1,55 @@
+//
+//  TabBarController.swift
+//  GameSpace
+//
+//  Created by Silvia Ferrer Rodriguez on 16/12/25.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabs()
+        setupAppearance()
+    }
+    
+    private func setupTabs() {
+        // home
+        let homeVc = HomeViewController()
+        let homeNav = UINavigationController(rootViewController: homeVc)
+        homeNav.tabBarItem = UITabBarItem(
+            title: "Home",
+            image: UIImage(systemName: "house"),
+            selectedImage: UIImage(systemName: "house.fill")
+        )
+        
+        // search
+        let searchVC = SearchViewController()
+        let searchNav = UINavigationController(rootViewController: searchVC)
+        searchNav.tabBarItem = UITabBarItem(
+            title: "Buscar",
+            image: UIImage(systemName: "magnifyingglass"),
+            selectedImage: UIImage(systemName: "magnifyingglass")
+        )
+        
+        // user
+        let userVC = UserViewController()
+        let userNav = UINavigationController(rootViewController: userVC)
+        userNav.tabBarItem = UITabBarItem(
+            title: "Perfil",
+            image: UIImage(systemName: "person"),
+            selectedImage: UIImage(systemName: "person.fill")
+        )
+        
+        viewControllers = [homeNav, searchNav, userNav]
+    }
+    
+    private func setupAppearance() {
+        tabBar.tintColor = .systemBlue
+        tabBar.backgroundColor = .systemBackground
+    }
+    
+    
+}
